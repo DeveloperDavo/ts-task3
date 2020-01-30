195e1b8f42721f3c239a8cf226d15bd7c980d0ce
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-01-30 19:16:55 +0100

    Add toggle

--- src/review/Reviews.tsx
+++ src/review/Reviews.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
+import Switch from '@material-ui/core/Switch'
 import { useAsyncRetry } from 'react-use'
 
 import { getReviews } from './review.service'
@@ -7,7 +8,10 @@ import ReviewsTable from './ReviewsTable'
 import { Review } from './types'
 
 export default function Reviews() {
-  const [toggleHighlightStartingFromRow1] = useState(false)
+  const [
+    toggleHighlightStartingFromRow1,
+    setToggleHighlightStartingFromRow1
+  ] = useState(false)
   const [reviews, setReviews] = useState<Review[] | null>(null)
 
   const { loading, error, retry } = useAsyncRetry(async () => {
@@ -21,6 +25,17 @@ export default function Reviews() {
         Refresh
       </Button>
       {loading && <> Loading...</>}
+      <div>
+        <Switch
+          checked={toggleHighlightStartingFromRow1}
+          onChange={() =>
+            setToggleHighlightStartingFromRow1(!toggleHighlightStartingFromRow1)
+          }
+          color="default"
+          value={toggleHighlightStartingFromRow1}
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+      </div>
       {error ? (
         <div>Error: {error.message}</div>
       ) : (
