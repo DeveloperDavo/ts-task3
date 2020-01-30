b876130756c6a4b37c49762dbb80982f3b052814
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-01-30 08:56:38 +0100

    Extract reviews table

--- src/Reviews.tsx
+++ src/Reviews.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
 import { useAsync } from 'react-use'
 
 import { getReviews } from './review.service'
-import { Review } from './types'
+import { ReviewsTable } from './ReviewsTable'
 
 export default function Reviews() {
   const { loading, error, value } = useAsync(async () => {
@@ -23,28 +16,7 @@ export default function Reviews() {
       ) : error ? (
         <div>Error: {error.message}</div>
       ) : (
-        value && (
-          <TableContainer component={Paper}>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left">Description</TableCell>
-                  <TableCell align="left">Comment</TableCell>
-                  <TableCell align="left">Creation Date</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {value.map((review: Review) => (
-                  <TableRow key={review.UID}>
-                    <TableCell align="left">{review.markDescription}</TableCell>
-                    <TableCell align="left">{review.comment}</TableCell>
-                    <TableCell align="left">{review.creationDate}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )
+        value && <ReviewsTable reviews={value} />
       )}
     </div>
   )
---
+++ src/ReviewsTable.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+
+import { Review } from './types'
+
+export function ReviewsTable({ reviews }: { reviews: Review[] }) {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Description</TableCell>
+            <TableCell align="left">Comment</TableCell>
+            <TableCell align="left">Creation Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {reviews.map((review: Review) => (
+            <TableRow key={review.UID}>
+              <TableCell align="left">{review.markDescription}</TableCell>
+              <TableCell align="left">{review.comment}</TableCell>
+              <TableCell align="left">{review.creationDate}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
