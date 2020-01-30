3823383e5eb0663fe9d9dccd2a6c4adb092b2463
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-01-30 08:46:53 +0100

    Convert getReviews to async function

--- src/review.service.ts
+++ src/review.service.ts
@@ -1,13 +1,9 @@
 import { Review } from './types'
 
-export function getReviews(): Promise<Review[]> {
-  return fetch(
+export async function getReviews(): Promise<Review[]> {
+  const response = await fetch(
     'https://api-qa.trustedshops.com/rest/internal/v2/shops/X6A4AACCD2C75E430381B2E1C4CLASSIC/reviews.json'
   )
-    .then(response => {
-      return response.json()
-    })
-    .then(json => {
-      return json.response.data.shop.reviews
-    })
+  const json = await response.json()
+  return json.response.data.shop.reviews
 }
