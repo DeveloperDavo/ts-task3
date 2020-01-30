d738bd822f1426d705d76381407678096dcd23f0
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-01-30 18:58:23 +0100

    Remove 'classnames'

--- src/ReviewsTable.tsx
+++ src/ReviewsTable.tsx
@@ -6,7 +6,6 @@ import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
-import classnames from 'classnames'
 
 import { Review } from './types'
 import styles from './ReviewsTable.module.css'
@@ -20,10 +19,8 @@ function rowClassName(
   toggleHighlightStartingFromRow1: boolean,
   i: number
 ): string | undefined {
-  return classnames({
-    [styles.highlight]:
-      (i + (toggleHighlightStartingFromRow1 ? 0 : 1)) % 2 === 0
-  })
+  if ((i + (toggleHighlightStartingFromRow1 ? 0 : 1)) % 2 === 0)
+    return styles.highlight
 }
 
 export function ReviewsTable({
