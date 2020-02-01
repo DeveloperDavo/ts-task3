f2c4b28690cf5ec94331d84c7ad41cbcc07d56b7
Developer Davo <DeveloperDavo@users.noreply.github.com>
2020-02-01 13:25:46 +0100

    Extract sortByMarkDescending

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,11 +1,16 @@
 import { Review } from './types'
 
+// assume mark can always be parsed as a float
+function sortByMarkDescending(): any {
+  return (a: Review, b: Review) => parseFloat(b.mark) - parseFloat(a.mark)
+}
+
 export async function getReviews(): Promise<Review[]> {
   const response = await fetch(
     'https://api-qa.trustedshops.com/rest/internal/v2/shops/X6A4AACCD2C75E430381B2E1C4CLASSIC/reviews.json'
   )
   const json = await response.json()
-  return json.response.data.shop.reviews.sort(
-    (a: Review, b: Review) => parseFloat(b.mark) - parseFloat(a.mark)
-  )
+  const reviews = json.response.data.shop.reviews
+  reviews.sort(sortByMarkDescending())
+  return reviews
 }
